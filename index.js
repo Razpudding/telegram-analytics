5912152e828ae19cd44de606fe2050caa853cd10
Laurens <laurens.aarnoudse@gmail.com>
2016-10-09 23:12:01 +0200

    First Commit! Two working visualisations
    Note that these will only work when you provide a tsv file with
    telegram data. Ill add an example file later.

---
+++ index.js
@@ -0,0 +1,191 @@
+/*
+* Created by Laurens Aarnoudse
+* TODO
+* Move to new folder, set up budo
+* Create graph per month with activity per person (change months with interactive button by using filter)
+* Anonimise data by deleting message content
+* Restructure so that index sets up the canvas, calls different class per chart (like linechart)
+*/
+
+//(function(){ disabled iffe for now to access vars from console TODO: turn back on
+
+var margin = {
+  top: 50,
+  right: 30,
+  bottom: 130,
+  left: 39
+};
+var width = 1200 - margin.left - margin.right;
+var height = 500 - margin.top - margin.bottom;
+
+var y = d3.scale.linear().range([height, 0]);
+var x = d3.scale.ordinal().rangeRoundBands([0, width], 0);
+var colorScale = d3.scale.linear().range(["lightblue", "blue"]);
+
+var barChart = d3.select(".chart")
+    .attr("y", "400")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+var lineChart = d3.select(".lineChart")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    
+lineChart.append("path")
+    .attr("class", "line");
+
+var globalData; //temporarily global to allow access from brower console
+
+d3.tsv("Tel_20_08_2015_2.tsv", function(error, data) {
+   var d3Groups = d3.nest()
+  .key(function(d) { return d.name; })
+  .entries(data);
+  //console.table(d3Groups);
+
+
+  //Let's group the data by name of the poster
+  data = groupMessages(data);
+  //var list = ["dirk", "dirkleton"];
+  var options = d3.select("#lineChartDropDown").selectAll('option').data(d3.keys(data)); // Data join\
+  // Enter selection
+  options.enter().append("option").text(function(d,i) { return d; });
+
+  buildLineChart(lineChart, data);
+
+  //console.log("loaded messages" + data.length);
+  //console.table(data);
+  x.domain(Object.keys(data));  //use Object.keys(myObject) to get an array of keys in the object
+  y.domain([0, d3.max(d3.values(data), function(d) { return d.length; } ) ]); //use d3.values to get an array of values from the object
+  colorScale.domain([0, d3.max(d3.values(data), function(d) { return d.averageActivity; } ) ]);  //This should be a calculation prob something like last message timestamp -first
+
+  var barWidth = width / x.domain().length;
+
+  //We're creating a group data join per datum. By tranlating the group to the right position,
+  //Its children wont need their x values set individually.
+  var bar = barChart.selectAll("g")
+      .data(d3.values(data))
+      .enter().append("g")
+      .attr("transform", function(d, i) {
+        return "translate(" + i * barWidth + ",0)";
+      });
+      
+    bar.append("rect")
+      //.attr("x", function(d,i) { return i * barWidth})
+      .attr("y", function(d) { return y(d.length) })
+      .attr("width", barWidth)
+      .attr("height", 0)
+      .attr("fill", function(d) { return colorScale(d.averageActivity)})  //Todo this should divide the #messages by the delta month between first and last message
+      .transition()
+        .delay(function(d,i) { return 150 * i})
+        .duration(300)
+        .ease("linear")
+        .attr("height", function(d) { return height - y(d.length)});
+
+    bar.selectAll("rect")
+    .append("svg:title")    //svg title adds a title element, which makes use of the browers native tooltip function
+      .text(function(d) { return d.averageActivity});
+
+    bar.append("text")
+      .attr("dx", "1em")
+      .attr("y", function(d) {
+        return y(d.length);
+      })
+      .attr("dy", "-1em")
+      .text(function(d) {
+        return d.length;
+      });
+
+    var yAxis = d3.svg.axis()
+      .scale(y)
+      .orient("left")
+      .ticks(10);
+
+    var xAxis = d3.svg.axis()
+      .scale(x)
+      .orient("bottom");
+
+    barChart.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis)
+    .selectAll("text")
+      .attr("dx", barWidth/2)
+      .attr("dy", "2em")
+      .attr("transform", "rotate(45)");
+
+    barChart.append("g")
+      .attr("class", "y axis")
+      .call(yAxis);
+});
+
+function groupMessages(messageData){
+  //Code adapted from: http://stackoverflow.com/questions/23864180/counting-data-items-in-d3-js
+  var groupedMessages = [];
+  for (var i = 0; i < messageData.length; i++){
+    var item = messageData[i];
+
+    if (!groupedMessages[item.name]){
+      groupedMessages[item.name] = [];
+    }
+    item.date = new Date(item.date.substr(4, 2)+"."+item.date.substr(1, 2)+"."+item.date.substr(7));
+    //IMPORTANT: if the date is invalid for whatever reason, make it equal to the previous date
+    //Even is this is a message by a different person, it will be the closest to the actual date
+    if (isNaN(item.date)){
+      item.date = messageData[i-1].date;
+    }
+
+    groupedMessages[item.name].push({date: item.date, message:item.message});
+  }
+
+  //Compute the deltatime between last and first post for each poster in months
+  for (var i in groupedMessages)
+  {
+    //TODO: Probably dont need this bit anymore since invalid dates are fixed above
+    var lastDate = groupedMessages[i][groupedMessages[i].length -1].date;
+    if (isNaN( lastDate.getTime()))
+    {
+      lastDate = groupedMessages[i][groupedMessages[i].length - 2].date;
+    }
+
+    groupedMessages[i].plusOne = 0;
+    groupedMessages[i].plusTwo = 0;
+    groupedMessages[i].exclamations = 0;
+    groupedMessages[i].questions = 0;
+
+    for (var j in groupedMessages[i]){
+      if (groupedMessages[i][j].message == undefined)
+      {
+        break;  //TODO: fix this dirty hack. this loop also includes properties like plusone which dont have a message component
+      }
+      if(groupedMessages[i][j].message.includes("!"))
+      {
+        groupedMessages[i].exclamations ++;
+      }
+      if(groupedMessages[i][j].message.includes("?"))
+      {
+        groupedMessages[i].questions ++;
+      }
+      if(groupedMessages[i][j].message.includes("+1"))
+      {
+        groupedMessages[i].plusOne ++;
+      }
+      else if(groupedMessages[i][j].message.includes("+2"))
+      {
+        groupedMessages[i].plusTwo ++;
+      }
+    }
+
+    //from milliseconds to seconds to mintes to hours to days to months
+    //+1 because posting once means you were active in one month
+    groupedMessages[i].monthsActive = 1 + (lastDate - groupedMessages[i][0].date)/1000 /60 /60 /24 /30;  
+    groupedMessages[i].averageActivity = groupedMessages[i].length / groupedMessages[i].monthsActive;
+  }
+  //console.table(groupedMessages);
+  globalData = groupedMessages;
+  return groupedMessages;
+}
+//}()); 
---
+++ lineChart.js
@@ -0,0 +1,111 @@
+/*
+* Created by Laurens Aarnoudse
+* This file will hold the linechart code
+* Steps:
+*	v 1. Try blocks example on codepen, change every line http://bl.ocks.org/mbostock/3883245
+*	V 2. Change blocks example to match my data
+*	V 3. Make chart work with my data
+*	4. Add additional data buckets to switch between posters or months
+*	TODO: Find a way to order data by days/weeks/or months
+*	TODO: Format data
+*/
+var messagesPerDay;
+
+function buildLineChart(svg, data)
+{	
+	var select  = d3.select("#lineChartDropDown").on("change", onChange);
+	console.log("building linechart in this svg: ");
+	console.log(svg);
+
+	var selectedIndex = d3.select("#lineChartDropDown").property('selectedIndex');
+	var name = d3.select("#lineChartDropDown").selectAll('option')[0][selectedIndex].text;
+	
+	// console.log("building linechart with this data");
+	// console.log(selectedData[28]);
+	
+	var margin = {top: 20, right: 20, bottom: 30, left: 50},
+	    width = 960 - margin.left - margin.right,
+	    height = 500 - margin.top - margin.bottom;
+
+	var x = d3.time.scale()
+	    .range([0, width]);
+
+	var y = d3.scale.linear()
+	    .range([height, 0]);
+
+	var xAxis = d3.svg.axis()
+	    .scale(x)
+	    .orient("bottom");
+
+	var yAxis = d3.svg.axis()
+	    .scale(y)
+	    .orient("left");
+
+	svg.append("g")
+	  .attr("class", "x axis")
+	  .attr("transform", "translate(0," + height + ")")
+	  .call(xAxis);
+
+	svg.append("g")
+	  .attr("class", "y axis")
+	  .call(yAxis)
+	.append("text")
+	  .attr("transform", "rotate(-90)")
+	  .attr("y", 6)
+	  .attr("dy", ".71em")
+	  .style("text-anchor", "end")
+	  .text("messages per day");
+
+	update(name);
+
+	function update(name)
+	{
+		console.log("update called with name: " + name);
+
+		var selectedData = countMessagesPerDay(data[name]);
+		selectedData.forEach(function(d) { d.date = new Date(d.key); });
+
+		var line = d3.svg.line()
+		    .x(function(d) { return x(d.date); })
+		    .y(function(d) { return y(d.values.length)} );
+		line.interpolate('step-after');
+
+		x.domain(d3.extent(selectedData,function(d) {return d.date;}));
+		y.domain(d3.extent(selectedData, function(d) { return d.values.length; }));
+		//console.log("xdomain: " + x.domain());
+		//console.log("ydomain: " + y.domain());
+
+		var body = d3.select("body").transition();	//This is a weird trick I dont fully understand
+		body.select(".lineChart .x.axis")
+            .duration(750)
+            .call(xAxis);
+        body.select(".lineChart .y.axis")
+            .duration(750)
+            .call(yAxis);
+
+		d3.select(".line")
+		  .datum(selectedData)
+		  .attr("d", line);
+	}
+
+	//adapted from https://stackoverflow.com/questions/11903709/adding-drop-down-menu-using-d3-js/11907096#11907096
+	function onChange() {
+		//console.log(select);
+		var selectedIndex = d3.select("#lineChartDropDown").property('selectedIndex');
+		var name          = d3.select("#lineChartDropDown").selectAll('option')[0][selectedIndex].text;
+		update(name); //This should prob be used to a more global control module
+	}
+}
+
+
+
+function countMessagesPerDay(messageData){
+	//console.log(messageData[0]);
+	messageData.forEach(function(d) { d.date.setHours(0,0,0,0); });
+
+	messagesPerDay = d3.nest()
+  		.key(function(d) { return d.date })
+  		.entries(messageData);
+  	//console.table(messagesPerDay);
+  	return messagesPerDay;
+}
